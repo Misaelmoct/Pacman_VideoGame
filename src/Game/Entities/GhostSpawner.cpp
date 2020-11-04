1ee7f80df06944ea092ca6c6fe89cfaf6c83a825
Misael_moct <misael.moctezuma@upr.edu>
2020-11-04 00:02:48 -0400

    clean

--- src/Game/Entities/GhostSpawner.cpp
+++ src/Game/Entities/GhostSpawner.cpp
@@ -12,19 +12,6 @@ GhostSpawner::GhostSpawner(int x, int y, int width, int height, ofImage spriteSh
     }
 }
 
-/*GhostSpawner::GhostSpawner(int x, int y, int width, int height, ofImage spriteSheet): Entity(x, y, width, height){
-
-    sprite1.cropFrom(spriteSheet, 456, 64, 16, 16)
-    Ghost* g1 = new Ghost(x, y, width, height, sprite1)
-    sprite1.cropFrom(spriteSheet, 456, 80, 16, 16)
-    Ghost* g1 = new Ghost(x, y, width, height, sprite1)
-    sprite1.cropFrom(spriteSheet, 456, 96, 16, 16)
-    Ghost* g1 = new Ghost(x, y, width, height, sprite1)
-    sprite1.cropFrom(spriteSheet, 456, 112, 16, 16)
-    Ghost* g1 = new Ghost(x, y, width, height, sprite1)
-
-}*/
-
 vector <Ghost*> GhostSpawner:: getGhostsVector(){
 
     return ghosts;
