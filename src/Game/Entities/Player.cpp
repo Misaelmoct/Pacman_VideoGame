8597871f03f036fcefda8a87cabc3e0212316200
Misael_moct <misael.moctezuma@upr.edu>
2020-11-04 18:08:57 -0400

    clean

--- src/Game/Entities/Player.cpp
+++ src/Game/Entities/Player.cpp
@@ -192,15 +192,10 @@ void Player::die(){
     this->health = this->health - 1;
     Entity::x = xSpawnPosition;
     Entity::y = ySpawnPosition;
-<<<<<<< HEAD
+
     dieSound.play();
-}
-=======
 }
 
 int Player:: getHealth(){
     return this->health;
 }
-
-
->>>>>>> Game-Over-Screen
