44dc9aaa41c8f9194d1c25bab43e71e9a39f58dc
LuisSantiago103 <luis.santiago103@upr.edu>
2020-11-07 00:10:20 -0400

    Fixed error in code

--- src/Game/Entities/Ghost.cpp
+++ src/Game/Entities/Ghost.cpp
@@ -5,7 +5,6 @@
 
 int Ghost:: ghostCount = 0;
 int Ghost:: uniqSpeed = 0;
-bool Player:: ghostIsWeak = false;
 
 Ghost:: Ghost(int x, int y, int width, int height, EntityManager* em):Entity(x, y, width, height){
 ghostSpawned.load("GHOST_SPAWN.mp3");
